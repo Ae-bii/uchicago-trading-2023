6f9f5993d65415c1aa0e9764b03a86bf4ae27676
Kevin Qiu <kzqiu01@gmail.com>
2023-04-07 22:49:02 -0400

    Added GRU/LSTM models for price forecasting.

--- clients/case1_bot.py
+++ clients/case1_bot.py
@@ -2,12 +2,14 @@
 
 from collections import defaultdict
 from typing import DefaultDict, Dict, Tuple
-from clients.utc_bot import UTCBot, start_bot
+from utc_bot import UTCBot, start_bot
 import math
-import clients.proto.utc_bot as pb
+from proto import utc_bot as pb
 import betterproto
 import asyncio
 import re
+import torch
+import torch.nn as nn
 
 DAYS_IN_MONTH = 21
 DAYS_IN_YEAR = 252
@@ -16,6 +18,80 @@ NUM_FUTURES = 14
 TICK_SIZE = 0.00001
 FUTURE_CODES = [chr(ord('A') + i) for i in range(NUM_FUTURES)] # Suffix of monthly future code
 CONTRACTS = ['SBL'] +  ['LBS' + c for c in FUTURE_CODES] + ['LLL']
+
+class GRUModel(nn.Module):
+    """GRUModel class extends nn.Module class and works as a constructor for GRUs.
+
+       GRUModel class initiates a GRU module based on PyTorch's nn.Module class.
+       It has only two methods, namely init() and forward(). While the init()
+       method initiates the model with the given input parameters, the forward()
+       method defines how the forward propagation needs to be calculated.
+       Since PyTorch automatically defines back propagation, there is no need
+       to define back propagation method.
+
+       Attributes:
+           hidden_dim (int): The number of nodes in each layer
+           layer_dim (str): The number of layers in the network
+           gru (nn.GRU): The GRU model constructed with the input parameters.
+           fc (nn.Linear): The fully connected layer to convert the final state
+                           of GRUs to our desired output shape.
+
+    """
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, dropout_prob):
+        """The __init__ method that initiates a GRU instance.
+
+        Args:
+            input_dim (int): The number of nodes in the input layer
+            hidden_dim (int): The number of nodes in each layer
+            layer_dim (int): The number of layers in the network
+            output_dim (int): The number of nodes in the output layer
+            dropout_prob (float): The probability of nodes being dropped out
+
+        """
+        super(GRUModel, self).__init__()
+
+        # Defining the number of layers and the nodes in each layer
+        self.layer_dim = layer_dim
+        self.hidden_dim = hidden_dim
+
+        # GRU layers
+        self.gru = nn.GRU(
+            input_dim, hidden_dim, layer_dim, batch_first=True, dropout=dropout_prob
+        )
+
+        # Fully connected layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        """The forward method takes input tensor x and does forward propagation
+
+        Args:
+            x (torch.Tensor): The input tensor of the shape (batch size, sequence length, input_dim)
+
+        Returns:
+            torch.Tensor: The output tensor of the shape (batch size, output_dim)
+
+        """
+        # Initializing hidden state for first input with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Forward propagation by passing in the input and hidden state into the model
+        out, _ = self.gru(x, h0.detach())
+
+        # Reshaping the outputs in the shape of (batch_size, seq_length, hidden_size)
+        # so that it can fit into the fully connected layer
+        out = out[:, -1, :]
+
+        # Convert the final state to our desired output shape (batch_size, output_dim)
+        out = self.fc(out)
+
+        return out
+
+# trained GRU model for price prediction
+gru = torch.load('../case1/analysis/models/gru.pt')
+
+def round_nearest(x, a):
+    return round(round(x / a) * a, -int(math.floor(math.log10(a))))             
 
 class Case1Bot(UTCBot):
     etf_suffix = ''
@@ -175,13 +251,6 @@ class Case1Bot(UTCBot):
                     )
 
                 self.__orders[f"underlying_{d}_{asset}"] = (r.order_id, order_px)
-                
-        
-
-def round_nearest(x, a):
-    return round(round(x / a) * a, -int(math.floor(math.log10(a))))             
-
-
 
 if __name__ == "__main__":
     start_bot(Case1Bot)
